64f7346d9eadc84baf5a843e7d7e390a4bd6a148
Peter Phan <peterphan0422@gmail.com>
2021-10-19 22:25:38 -0500

    feat: user and chef create route refactored

--- src/routes/userRoutes/authenticationUsersRoutes.js
+++ src/routes/userRoutes/authenticationUsersRoutes.js
@@ -7,59 +7,76 @@ var crypto = require("crypto");
 module.exports = async function authenticateUsers(fastify) {
 	fastify.addContentTypeParser("multipart/form-data", (request, payload, done) => done());
 
-	fastify.post("/auth/user-create", async (request, reply) => {
-		const busboy = new Busboy({ headers: request.headers });
-		request.raw.pipe(busboy);
+	fastify.post("/auth/user", async (request, reply) => {
+		const { firstname, lastname, email, password, address, phone, avatarURL } = request.body;
 
-		const bucketParams = { Bucket: "dashchef-dev" };
-		const dataObj = {};
+		if (!firstname || !lastname || !email || !password || !address || !phone) {
+			return { code: 400, message: "Missing values, please check input fields." };
+		}
 
-		busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-			bucketParams.Key = crypto.randomBytes(20).toString("hex");
-			bucketParams.ContentType = mimetype;
-
-			const fileBuffers = [];
-			file.on("data", (data) => fileBuffers.push(data));
-
-			file.on("end", async () => {
-				const file = Buffer.concat(fileBuffers);
-				bucketParams.Body = file;
-			});
-		});
-
-		busboy.on("field", (fieldname, val) => {
-			dataObj[fieldname] = val;
-		});
-
-		busboy.on("finish", async () => {
-			try {
-				const s3res = await s3Client.send(new PutObjectCommand(bucketParams));
-				if (s3res.$metadata.httpStatusCode !== 200) {
-					reply.code(400).send({ message: "Failed to post image to s3" });
-				}
-				dataObj.avatarURL = process.env.BASE_S3_URL + bucketParams.Key;
-				const { firstname, lastname, email, password, address, phone, avatarURL } = dataObj;
-
-				if (!firstname || !lastname || !email || !password || !address || !phone) {
-					return { code: 400, message: "Missing values, please check input fields." };
-				}
-
-				const hash = await bcrypt.hash(password, 10);
-				const client = await fastify.pg.connect();
-				await client.query(
-					"INSERT INTO users (first_name, last_name, email, password, address, phone, avatar_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;",
-					[firstname, lastname, email, hash, address, phone, avatarURL]
-				);
-				client.release();
-				reply.code(201).send({ message: "User successful created!" });
-			} catch (err) {
-				console.log("Error", err);
-				reply.code(400).send({ message: "Error, something went wrong :( " });
-			}
-		});
+		const hash = await bcrypt.hash(password, 10);
+		const client = await fastify.pg.connect();
+		await client.query(
+			"INSERT INTO users (first_name, last_name, email, password, address, phone, avatar_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;",
+			[firstname, lastname, email, hash, address, phone, avatarURL]
+		);
+		client.release();
+		reply.code(201).send({ message: "User successful created!" });
 	});
 
-	fastify.post("/auth/user-login", async (request, reply) => {
+	// fastify.post("/auth/user-image", async (request, reply) => {
+	// 	const busboy = new Busboy({ headers: request.headers });
+	// 	request.raw.pipe(busboy);
+
+	// 	const bucketParams = { Bucket: "dashchef-dev" };
+	// 	const dataObj = {};
+
+	// 	busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+	// 		bucketParams.Key = crypto.randomBytes(20).toString("hex");
+	// 		bucketParams.ContentType = mimetype;
+
+	// 		const fileBuffers = [];
+	// 		file.on("data", (data) => fileBuffers.push(data));
+
+	// 		file.on("end", async () => {
+	// 			const file = Buffer.concat(fileBuffers);
+	// 			bucketParams.Body = file;
+	// 		});
+	// 	});
+
+	// 	busboy.on("field", (fieldname, val) => {
+	// 		dataObj[fieldname] = val;
+	// 	});
+
+	// 	busboy.on("finish", async () => {
+	// 		try {
+	// 			const s3res = await s3Client.send(new PutObjectCommand(bucketParams));
+	// 			if (s3res.$metadata.httpStatusCode !== 200) {
+	// 				reply.code(400).send({ message: "Failed to post image to s3" });
+	// 			}
+	// 			dataObj.avatarURL = process.env.BASE_S3_URL + bucketParams.Key;
+	// 			const { firstname, lastname, email, password, address, phone, avatarURL } = dataObj;
+
+	// 			if (!firstname || !lastname || !email || !password || !address || !phone) {
+	// 				return { code: 400, message: "Missing values, please check input fields." };
+	// 			}
+
+	// 			const hash = await bcrypt.hash(password, 10);
+	// 			const client = await fastify.pg.connect();
+	// 			await client.query(
+	// 				"INSERT INTO users (first_name, last_name, email, password, address, phone, avatar_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;",
+	// 				[firstname, lastname, email, hash, address, phone, avatarURL]
+	// 			);
+	// 			client.release();
+	// 			reply.code(201).send({ message: "User successful created!" });
+	// 		} catch (err) {
+	// 			console.log("Error", err);
+	// 			reply.code(400).send({ message: "Error, something went wrong :( " });
+	// 		}
+	// 	});
+	// });
+
+	fastify.post("/auth/user/login", async (request, reply) => {
 		const { email, password } = request.body;
 
 		if (!email) {
@@ -67,7 +84,10 @@ module.exports = async function authenticateUsers(fastify) {
 		}
 
 		const client = await fastify.pg.connect();
-		const { rows } = await client.query("SELECT password, id FROM users WHERE email=$1", [email]);
+		const { rows } = await client.query(
+			'SELECT password, id, first_name AS "firstname", last_name AS "lastname" FROM users WHERE email=$1',
+			[email]
+		);
 		client.release();
 		const passwordMatch = await bcrypt.compare(password, rows[0].password);
 		if (passwordMatch) {
@@ -77,7 +97,14 @@ module.exports = async function authenticateUsers(fastify) {
 				password,
 				id: rows[0].id,
 			});
-			return { code: 200, message: "Successfully logged in!", id: rows[0].id, token };
+			return {
+				code: 200,
+				message: "Successfully logged in!",
+				id: rows[0].id,
+				firstName: rows[0].firstname,
+				lastname: rows[0].lastname,
+				token,
+			};
 		}
 		return { code: 400, message: "Incorrect credentials, please try again!" };
 	});
